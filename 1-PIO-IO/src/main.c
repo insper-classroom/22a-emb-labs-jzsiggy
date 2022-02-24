d90e02aa6656237a754d7dd2b824571b3b2bc3a6
Joao Zsigmond <jzsiggy@gmail.com>
2022-02-24 15:14:27 -0300

    initial commit

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,52 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO           PIOC
+#define LED_PIO_ID        ID_PIOC
+#define LED_PIO_IDX       8
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+
+#define BUT_PIO				PIOA
+#define BUT_PIO_ID			ID_PIOA
+#define BUT_PIO_IDX			11
+#define BUT_PIO_IDX_MASK (1 << BUT_PIO_IDX)
+
+/************************************************************************/
+
+#define LED1_PIO           PIOA
+#define LED1_PIO_ID        ID_PIOA
+#define LED1_PIO_IDX       0
+#define LED1_PIO_IDX_MASK  (1 << LED1_PIO_IDX)
+
+#define BUT1_PIO			PIOD
+#define BUT1_PIO_ID			ID_PIOD
+#define BUT1_PIO_IDX		28
+#define BUT1_PIO_IDX_MASK (1 << BUT1_PIO_IDX)
+
+/************************************************************************/
+
+#define LED2_PIO           PIOC
+#define LED2_PIO_ID        ID_PIOC
+#define LED2_PIO_IDX       30
+#define LED2_PIO_IDX_MASK  (1 << LED2_PIO_IDX)
+
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			ID_PIOC
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK	(1 << BUT2_PIO_IDX)
+
+/************************************************************************/
+
+#define LED3_PIO           PIOB
+#define LED3_PIO_ID        ID_PIOB
+#define LED3_PIO_IDX       2
+#define LED3_PIO_IDX_MASK  (1 << LED3_PIO_IDX)
+
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			ID_PIOA
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK	(1 << BUT3_PIO_IDX)
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -41,6 +83,8 @@
 /************************************************************************/
 
 void init(void);
+void apaga(Pio*, int);
+void pisca(int, Pio*, int);
 
 /************************************************************************/
 /* interrupcoes                                                         */
@@ -51,9 +95,51 @@ void init(void);
 /************************************************************************/
 
 // Função de inicialização do uC
-void init(void)
-{
+void init(void){
+	// Initialize the board clock
+	sysclk_init();
 
+	// Disativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+
+	//Inicializa PC8 como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+	
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP);
+	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_PULLUP);
+	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_PULLUP);
+	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_PULLUP);
+}
+
+void pisca(int n, Pio* led_pio, int led_pio_idx) {
+	for (int i = 0; i < n; i++) {
+		pio_clear(led_pio, (1 << led_pio_idx));
+		delay_ms(200);                        
+		pio_set(led_pio, (1 << led_pio_idx));
+		delay_ms(200);
+	};
+};
+
+void apaga(Pio* led_pio, int led_pio_idx) {
+	pio_set(led_pio, (1 << led_pio_idx));
 }
 
 /************************************************************************/
@@ -61,15 +147,38 @@ void init(void)
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
-int main(void)
-{
-  init();
-
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
-
-  }
-  return 0;
+int main(void) {
+	// inicializa sistema e IOs
+	init();
+	
+	while (1) {
+		if (!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
+			pisca(5, LED1_PIO, LED1_PIO_IDX);
+		} 
+		else {
+			apaga(LED1_PIO, LED1_PIO_IDX);
+		}
+		
+		if (!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)) {
+			pisca(5, LED2_PIO, LED2_PIO_IDX);
+		} else {
+			apaga(LED2_PIO, LED2_PIO_IDX);
+		}
+		
+		if (!pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK)) {
+			pisca(5, LED3_PIO, LED3_PIO_IDX);
+		}
+		else {
+			apaga(LED3_PIO, LED3_PIO_IDX);
+		};
+		
+		if (!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+			pisca(5, LED_PIO, LED_PIO_IDX);
+		}
+		else {
+			apaga(LED_PIO, LED_PIO_IDX);
+		};
+	}
+	
+	return 0;
 }
